c6cb0ba4296cd10fb1d3d2b91d588d22537d3c26
mani_ <mani_@GowsiMani>
2023-10-21 12:02:20 +0530

    My 1st commit to git

---
+++ src/GitTestPck/ClsGitCheck.java
@@ -0,0 +1,10 @@
+package GitTestPck;
+
+public class ClsGitCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Git welcome to IT world");
+	}
+
+}
