4a69307c05e9dd5427bc65dc1ff93ff69b4c292b
ManikandanThavamani <148377442+ManikandanThavamani@users.noreply.github.com>
2023-10-21 12:43:25 +0530

    Update ClsGitCheck.java
    test

--- src/GitTestPck/ClsGitCheck.java
+++ src/GitTestPck/ClsGitCheck.java
@@ -4,7 +4,7 @@ public class ClsGitCheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Git welcome to IT world");
+		System.out.println("Test Git welcome to IT world new");
 		//For test
 		for (int i=0;i<=3;i++) {
 			System.out.println("Hello world" + i);
