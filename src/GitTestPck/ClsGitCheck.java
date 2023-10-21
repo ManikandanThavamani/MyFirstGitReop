ec63e7a830550921385a71ef5d9675e9ff9cc3c1
mani_ <mani_@GowsiMani>
2023-10-21 12:08:51 +0530

    2nd commit to git for loop added

--- src/GitTestPck/ClsGitCheck.java
+++ src/GitTestPck/ClsGitCheck.java
@@ -5,6 +5,11 @@ public class ClsGitCheck {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Git welcome to IT world");
+		//For test
+		for (int i=0;i<=3;i++) {
+			System.out.println("Hello world" + i);
+		}
 	}
+	
 
 }
